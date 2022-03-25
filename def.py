27bfaa6bd1d7a2d71505a0c622355ef28eb41d1d
SandwichBlamwich <84096188+SandwichBlamwich@users.noreply.github.com>
2022-03-25 13:22:40 -0500

    def.py made

---
+++ def.py
@@ -0,0 +1,8 @@
+def mydef():
+	print("this is my def function!")
+	Fun = input("def> ")
+	if Fun == "what?":
+		print("a def is a callable string of code!")
+	else:
+		print("ah never mind.")
+mydef()
