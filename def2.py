ac753ba1f4949c40beb57f4a93bfbbaf0aad419b
SandwichBlamwich <84096188+SandwichBlamwich@users.noreply.github.com>
2022-03-25 13:23:33 -0500

    def2.py made

---
+++ def2.py
@@ -0,0 +1,11 @@
+print('welcome to my code! if you type “def” you will see my new def! if you dont, you will still see it! :D')
+startgame = input('>>>')
+
+def myNewDef():
+  print('this is a def')
+  fun = input('def> ')
+  if fun == 'what?':
+    print('a def is a callable string of code!')
+  else:
+    print('ah, never mind.')
+myNewDef()
