ee75d6d8fa1e954918e7b6742cb59f52ebb04be5
SandwichBlamwich <84096188+SandwichBlamwich@users.noreply.github.com>
2022-03-28 13:53:08 -0500

    created comments.py

---
+++ comments.py
@@ -0,0 +1,18 @@
+# this is a comment. it will not print.
+'''
+this 
+is
+a
+multiline
+comment.
+it will
+not print!
+'''
+
+print('I will print!')
+# comments can be used to tell people what code means, or make notes for yourself!
+
+# shopping list variable.
+shopping_list = 'eggs','milk','cheese','bread'
+#now to print the shopping list:
+print(shopping_list)
