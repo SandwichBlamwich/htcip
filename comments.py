125d2a91f6ab63e0b8e8e09d098ec300526922a1
SandwichBlamwich <84096188+SandwichBlamwich@users.noreply.github.com>
2022-03-30 13:53:53 -0500

    updated comments.py

--- comments.py
+++ comments.py
@@ -1,18 +1,28 @@
-# this is a comment. it will not print.
+# adding a hashtag means that the text after it will not print or mean anything! it is a COMMENT!
+
+# print('this is a comment. it will not print.')
+
 '''
 this 
 is
 a
 multiline
-comment.
-it will
-not print!
+comment
+for
+long
+messages
 '''
 
-print('I will print!')
-# comments can be used to tell people what code means, or make notes for yourself!
+print('I will print!') 
+# comments can be used to tell people what code means, or make notes for yourself! (the above code is a print.)
 
-# shopping list variable.
+# shopping list variable:
 shopping_list = 'eggs','milk','cheese','bread'
-#now to print the shopping list:
+#now to print the shopping list so I can remember.
 print(shopping_list)
+
+'''
+if you ever need to use this in your code, go ahead!
+it's a very nice addition to your code!
+bye now!
+'''
